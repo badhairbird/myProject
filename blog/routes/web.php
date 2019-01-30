5e2e27f7fa10e094d2cbdb720a9c6267aa24fe63
wenxing74 <badhairbird@gmail.com>
2019-01-30 17:52:24 +0800

    third

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,4 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('tasks', 'TaskController');Route::resource('tasks', 'TaskController');
+Route::resource('tasks', 'TaskController');
+
+Route::resource('todos', 'TodoController');
